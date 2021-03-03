e06d9cd9de20623231fd8e7a7bc037e0726a234f
TeodoraHoamea <teodora.hoamea@gmail.com>
2021-03-03 20:46:00 +0200

    Added consoleapp to the solution

---
+++ ConsoleAppTAPC2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTAPC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
